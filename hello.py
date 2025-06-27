82c456671548c0be47e821efaac23bdb74d890d0
ramcharan-21 <ramcharankadiyala21@gmail.com>
2025-06-27 16:05:59 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello from Ranga Bharadwaj's DevOps Project 57E!")
+    print("Hello from Ramcharan 528!")
+    print("Hello from Ranga Bharadwaj 57E!")
+    print("Hello from Ravindra 57H!")
+    
 
 if __name__ == "__main__":
     main()
