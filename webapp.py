39449f3f42b1e6e0b7ddf994d71b9031d8232226
ramcharan-21 <ramcharankadiyala21@gmail.com>
2025-06-27 16:04:21 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,4 +3,6 @@
 import streamlit as st
 
 st.title("Streamlit Name App – DevOps Lab")
-st.write("Hello from Ranga Bharadwaj!")
+st.write("Hello from Ramcharan! 24955A0528")
+st.write("Hello from Ranga Bharadwaj! 23951A057E")
+st.write("Hello from Ravindra! 23951A057H")
